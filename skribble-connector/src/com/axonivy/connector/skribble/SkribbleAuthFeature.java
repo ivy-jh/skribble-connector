5cb1e43608d87f63df238b2e83929bc951623a2a
ivy-jh <jens.hartmann@axonivy.com>
2024-08-26 17:07:45 +0200

    initialize edited openapi: renamed all pathstart with '/v2/..'

--- skribble-connector/src/com/axonivy/connector/skribble/SkribbleAuthFeature.java
+++ skribble-connector/src/com/axonivy/connector/skribble/SkribbleAuthFeature.java
@@ -14,11 +14,15 @@ import javax.ws.rs.core.MediaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.skribble.api.v2.client.AccessLoginBody;
 
+import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.rest.client.FeatureConfig;
 
 public class SkribbleAuthFeature implements Feature {
 
+	private static String sessionToken = null;
+	private static boolean isSessionTokenAlive = false;
+	
   @Override
   public boolean configure(FeatureContext context) {
     context.register(new AuthFilter(), Priorities.AUTHENTICATION);
@@ -29,7 +33,7 @@ public class SkribbleAuthFeature implements Feature {
 
     @Override
     public void filter(ClientRequestContext ctxt) throws IOException {
-      if (ctxt.getUri().toASCIIString().contains("/login")) {
+      if (ctxt.getUri().toASCIIString().contains("/login") || ctxt.getUri().toASCIIString().contains("/health") ) {
     	  return;
       }
     	
@@ -40,19 +44,40 @@ public class SkribbleAuthFeature implements Feature {
       login.setApiKey(key);
       login.setUsername(config.readMandatory("AUTH.username"));
       
-      var token = "";//config.readMandatory("AUTH.sessionToken");
       
-      if(token.isEmpty()) {
-    	  token = Ivy.rest().client(UUID.fromString("6c92d3d7-cb33-4cfa-964a-8242ad165742"))
-    		  		.path("/access/login").request()
+      try {
+    	  sessionToken = (String) IApplication.current().getAttribute("skribble.auth.sessionToken");
+          Ivy.log().info("sessionToken from Application: "+sessionToken);
+    	  
+          var get = Ivy.rest().client(UUID.fromString("6c92d3d7-cb33-4cfa-964a-8242ad165742"))
+        		  	.path("/management/health").request()
+        		  	.accept("Accept: */*")
+        		  	.header("Authorization", "Bearer "+sessionToken)
+		  			.get();
+          
+          Ivy.log().info("get: "+get.readEntity(String.class));
+          
+          isSessionTokenAlive = get.getStatus() == 200 ? true: false;
+          
+          Ivy.log().info("is Token alive: "+isSessionTokenAlive);
+          
+      }catch(Exception e){
+    	  isSessionTokenAlive = false;
+      }
+      
+      //var sessionToken = "";   //config.readMandatory("AUTH.sessionToken"); ivy.session. // ivyapp
+      
+      if(sessionToken == null || !isSessionTokenAlive) {
+    	  sessionToken = Ivy.rest().client(UUID.fromString("6c92d3d7-cb33-4cfa-964a-8242ad165742"))
+    		  		.path("v2/access/login").request()
     		  		.post(Entity.entity(login, MediaType.APPLICATION_JSON), String.class);
     	  
-    	  Ivy.log().info("got token "+token);
-    	  Ivy.var().set("ivy.var.skribbleConnector.sessionToken", token);
+    	  Ivy.log().info("got token "+sessionToken);
+    	  IApplication.current().setAttribute("skribble.auth.sessionToken",sessionToken );
     	  
       }
       
-      ctxt.getHeaders().putSingle("Authorization", "Bearer "+token);
+      ctxt.getHeaders().putSingle("Authorization", "Bearer "+sessionToken);
     }
     
     
---
+++ skribble-connector/src/com/skribble/openapi/bean/DateTimeHelperBean.java
@@ -0,0 +1,29 @@
+package com.skribble.openapi.bean;
+
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import ch.ivyteam.ivy.environment.Ivy;
+
+
+@ViewScoped
+@ManagedBean
+public class DateTimeHelperBean{
+	
+    public static Date getJavaDate(String instantStr) {
+    	
+    	Date date = null;
+    	try {
+    		date = Date.from(java.time.Instant.parse(instantStr));
+    		
+	   	}catch(Exception e){
+	   		Ivy.log().warn(e);
+	   	}
+
+    	return date;
+    		
+    }      
+}
